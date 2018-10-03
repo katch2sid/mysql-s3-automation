02b4f8e9b74d1b7615d24d39c7361c9d0b5ccd3d
Siddharth Sinha <katch2sid@gmail.com>
2018-10-03 13:57:08 +0530

    Initial Code
    This would create a SQL Backup and further push to the s3 bucket

---
+++ s3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+AWS_ACCESS_KEY_ID=my_key \
+AWS_SECRET_ACCESS_KEY=my_secret \
+AWS_DEFAULT_REGION=us-west-2 \
+S3_BUCKET=my_bucket \
+MYSQL_HOST=host \
+MYSQL_PORT=port \
+MYSQL_USER=myuser \
+MYSQL_PASS=pass \
+MYSQL_DB=db \
+backup.sh
